2c8d863f53b356192ccad3e7c8ddb7078d026a20
Abdul Basit <abdul_basit313@outlook.com>
2020-06-20 02:36:53 +0500

    changed tabs icon

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import ChatIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import PeopleIcon from 'react-native-vector-icons/MaterialIcons'
 
 import { ChatStackScreen, PeopleStackScreen } from './stack';
 import colors from '../config/colors';
@@ -12,19 +13,6 @@ const Root = () => {
   return (
     <NavigationContainer>
       <AppTab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName
-
-            if (route.name === 'Chats') {
-              iconName = 'chat'
-            }
-            else if (route.name === 'People') {
-              iconName = 'nature-people'
-            }
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />
-          }
-        })}
         tabBarOptions={{
           style: {
             backgroundColor: colors.background,
@@ -34,8 +22,18 @@ const Root = () => {
           inactiveTintColor: colors.subTitle,
         }}
       >
-        <AppTab.Screen name='Chats' component={ChatStackScreen} />
-        <AppTab.Screen name='People' component={PeopleStackScreen} />
+        <AppTab.Screen
+          name='Chats'
+          component={ChatStackScreen}
+          options={{
+            tabBarIcon: ({ size, color }) => <ChatIcon name='chat' size={size} color={color} />
+          }} />
+        <AppTab.Screen
+          name='People'
+          component={PeopleStackScreen}
+          options={{
+            tabBarIcon: ({ size, color }) => <PeopleIcon name='people' size={size} color={color} />
+          }} />
       </AppTab.Navigator>
     </NavigationContainer >
   )
